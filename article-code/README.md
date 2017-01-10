3e018345b8623dec31d3b75db2aa2f817ca9c935
Darius Bacon <darius@wry.me>
2017-01-09 18:37:07 -0800

    add handaxeweb

--- article-code/README.md
+++ article-code/README.md
@@ -1,6 +1,6 @@
 Here are source files extracted from my article on compiling
 Python. There are three versions of the compiler, each handling a
-larger subset of Python. To run one, you need Python 3.4 or above. You
+larger subset of Python. To run one, you need Python 3.4. You
 also need `check_subset.py` from the parent directory; copy it down
 here. Then this should work:
 
@@ -13,3 +13,10 @@ The final version of the compiler from the article, `tailbiter2.py`,
 is almost the same as `../compiler.py` but missing a few features
 needed to compile `byterun`. These smaller versions are provided just
 so you can easily run the exact code from the article.
+
+`handaxeweb.py` extracted the tailbiter source from the article's
+Markdown source; since the article is distributed as HTML this is
+useless to you, but perhaps someone might adapt it for their own
+literate programming, so here you are. It's derived from an earlier
+version in Python of Kragen Sitaker's
+[handaxeweb](https://github.com/kragen/peg-bootstrap/blob/master/handaxeweb.md).
---
+++ article-code/handaxeweb.py
@@ -0,0 +1,73 @@
+"""
+Original by Kragen Sitaker.
+Now also supports versions of the form "v2+".
+"""
+
+import re, sys
+
+new_chunk_pattern = re.compile(r'# in (.*?):\s*$')
+chunk_ref_pattern = re.compile(r'(?m)^([ \t]*)<<(.*)>>[ \t]*\n')
+
+def main(argv, infile):
+    chunks = parse(infile)
+    if argv[1:] == ['--list']:
+        for name in sorted(find_roots(chunks)):
+            print name
+    else:
+        if not argv[1:]:
+            sys.stdout.write(expand(chunks, '*'))
+        else:
+            name, version = (argv[1], '0') if len(argv) == 2 else argv[1:]
+            v = int(version)
+            filename = '%d/%s' % (v, name)
+            with open(filename, 'w') as outfile:
+                outfile.write(expand(chunks, name, v))
+
+def parse(infile):
+    "Make a chunks table from :infile."
+    chunk_name, chunks = '*', {}
+    lines = iter(infile)
+    for line in lines:
+        if line.startswith("```"):
+            line = next(lines)
+            new_chunk = new_chunk_pattern.match(line)
+            if new_chunk:
+                chunk_name = new_chunk.group(1)
+                line = next(lines)
+            while line.rstrip() != "```":
+                chunks[chunk_name] = chunks.get(chunk_name, '') + line
+                line = next(lines)
+    return chunks
+
+def find_roots(chunks):
+    "Return a set of the chunk_names that aren't referenced in chunks."
+    chunk_refs = {name
+                  for s in chunks.values()
+                  for indent, name in chunk_ref_pattern.findall(s)}
+    return set(chunks.keys()) - chunk_refs
+
+def expand(chunks, name, version):
+    "Return the named chunk with any chunk-references recursively expanded."
+    template, latest = '', -1
+    for v in range(version+1):
+        t = chunks.get(name + ' v%d+' % v, '')
+        if t:
+            latest = v
+            template += t
+    for v in range(version, latest, -1):
+        t = chunks.get(name + ' v%d' % v, '')
+        if t:
+            template += t
+            break
+    if not template:
+        template = chunks[name]
+    return chunk_ref_pattern.sub(
+        lambda mo: indent(mo.group(1), expand(chunks, mo.group(2), version)),
+        template)
+
+def indent(dent, text):
+    "Return :text with :dent prepended to every line."
+    return re.sub(r'(?m)^(?=.)', dent, text) # (?=.) to exclude \Z
+
+if __name__ == '__main__':
+    main(sys.argv, sys.stdin)
